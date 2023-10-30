5ed2760cce0d98bec2b616dc9888d11146e66a1e
bayukrisna <bayukrisna14658@gmail.com>
2023-10-30 15:16:59 +0700

    memperjelas isi codingan

--- app/src/main/java/com/bayu/junit/luas_persegipanjang.java
+++ app/src/main/java/com/bayu/junit/luas_persegipanjang.java
@@ -1,7 +1,11 @@
 package com.bayu.junit;
 
+
+//membuat class luas_persegipanjang
 public class luas_persegipanjang {
+    //membuat fungsi luas_add lalu isi dengan parameter
     public int luas_add(int alas,int tinggi){
+        // karena rumus luas persegi panjang adalah panjang x lebar maka membuat return dengan alas * tinggi
         return alas * tinggi;
     }
 }
--- app/src/test/java/com/bayu/junit/ExampleUnitTest.java
+++ app/src/test/java/com/bayu/junit/ExampleUnitTest.java
@@ -7,9 +7,13 @@ import static org.junit.Assert.*;
 
 public class ExampleUnitTest {
     @Test
+    // membuat fungsi
     public void addition_isCorrect() {
+        //memanggil class luas_persegipanjang lalu membuat objek a
         luas_persegipanjang a = new luas_persegipanjang();
+        // lalu buat variabel, memanggil objek dan memanggil parameter pada class luas_persegipanjang
         int hasil = a.luas_add(5,2);
+        //assertEqual digunakan untuk menegaskan apakah hasil dari variabel hasil sama dengan isi pada assertEquals
         assertEquals(10, hasil);
     }
 }
